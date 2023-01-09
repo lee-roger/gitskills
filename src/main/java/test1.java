97892c4384398f02a751e12ae5200605b472bce2
roger-lee <1449162254@qq.com>
2023-01-09 22:20:05 +0800

    Revert "第三次修改"
    This reverts commit 0acaef88

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -7,7 +7,5 @@ public class test1 {
         System.out.println(a);
         System.out.println("测试文件");
         System.out.println("第三次修改");
-        System.out.println("热修改");
-        System.out.println("push test");
     }
 }
